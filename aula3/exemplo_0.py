f0feee5811869fb85e194009d28a357766468cec
José Filho <100032235+josecsfilho@users.noreply.github.com>
2022-09-09 20:27:54 -0300

    Add files via upload

---
+++ aula3/exemplo_0.py
@@ -0,0 +1,15 @@
+
+#----------------------------------------------------
+#nome = input('Qual o seu nome? ')
+#print(f'O usuário digitou {nome} e o tipo da variável é '
+#      f'{type(nome)}')
+#----------------------------------------------------
+nome = input('Qual o seu nome? ')
+idade = input('Qual sua idade? ')
+
+ano_nascimento = 2022-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos.'
+      f'{nome} nasceu em {ano_nascimento}')
+
---
+++ aula3/exemplo_1_0.py
@@ -0,0 +1,9 @@
+
+
+if True:
+    print('Verdadeiro.')
+
+    num_1 = 2
+    num_2  = 4
+
+    print(num_1 == num_2)
