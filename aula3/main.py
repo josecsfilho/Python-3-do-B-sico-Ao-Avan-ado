4c8b990965724e39fa7df08102f214eae87a192c
José Filho <100032235+josecsfilho@users.noreply.github.com>
2022-09-12 21:42:59 -0300

    Add files via upload

---
+++ aula3/main.py
@@ -0,0 +1,18 @@
+ 
+nome = input('Qual o seu nome? ')
+
+if nome:
+    print(nome)
+else:
+    print('Você não digitou nada =(')
+
+
+#a = 0
+#b = None
+#c = False
+#d = []
+#e = {}
+#f = 22
+#g = 'José'
+
+#variavel = a or b or c or d or e or f or g
