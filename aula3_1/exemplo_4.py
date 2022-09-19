40ca0a328f3879251cb3e12ca6169be3a9c20f56
José Filho <100032235+josecsfilho@users.noreply.github.com>
2022-09-18 21:35:27 -0300

    Add files via upload

---
+++ aula3_1/exemplo_4.py
@@ -0,0 +1,21 @@
+
+
+#def funcao(arg, arg2):
+#    return arg * arg2
+
+#var = funcao(2,2)
+
+#a = lambda  x, y: x * y
+
+#print(a(2,2))
+
+
+lista = [
+    ['P1', 13],
+    ['P2', 6],
+    ['P3', 7],
+    ['P4', 50],
+    ['P5', 8],
+]
+lista.sort(key=lambda item: item[1])
+print(lista)
