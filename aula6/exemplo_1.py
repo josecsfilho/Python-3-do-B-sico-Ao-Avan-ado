dc616d8f57d2026d47495c702a10b18a0d5df7f2
José Filho <100032235+josecsfilho@users.noreply.github.com>
2022-09-24 23:06:32 -0300

    Add files via upload

---
+++ aula6/exemplo_1.py
@@ -0,0 +1,17 @@
+
+
+import sys
+import time
+
+def gera():
+    variavel = 'Valor 1'
+    yield variavel
+    variavel = 'Valor 2'
+    yield variavel
+    variavel = 'Valor 3'
+    yield variavel
+
+g = gera()
+
+for v in g:
+    print(v)
